a8a45417ba2bf27decbc7b4025891d88b9b7dcb3
Dmitry Beznosiuk <145875719+DmitryBizCode@users.noreply.github.com>
2024-11-06 20:32:34 +0200

    all done

---
+++ src/Services/SQLService.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Services;
+use PDO;
+use PDOException;
+
+class SQLService
+{
+    private PDO $pdo;
+
+    public function __construct(string $host = 'localhost', string $user = 'root', string $pass = 'root', string $db = 'blog', int $port = 3306)
+    {
+        try {
+            $this->pdo = new PDO("mysql:host=$host;port=$port;dbname=$db", $user, $pass);
+        } catch (PDOException $e) {
+            echo "Error access to database: " . $e->getMessage();
+        }
+    }
+
+    public function getPdo(): PDO
+    {
+        return $this->pdo;
+    }
+
+}
